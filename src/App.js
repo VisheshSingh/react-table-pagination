c2fc6845ce2cd5f338996b4571b2ced0398cbeca
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-05 22:56:05 -0500

    React-bootstrap table POC 💷

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, {
+  textFilter,
+  selectFilter,
+} from 'react-bootstrap-table2-filter';
 
-function App() {
+const App = () => {
+  const [players, setPlayers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchPlayers = async () => {
+    const res = await fetch(`https://nba-players.herokuapp.com/players-stats`);
+    const data = await res.json();
+    setPlayers(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  const columns = [
+    {
+      dataField: 'name',
+      text: 'Player name',
+      filter: textFilter(),
+    },
+    {
+      dataField: 'team_name',
+      text: 'Team',
+      filter: textFilter(),
+    },
+    {
+      dataField: 'points_per_game',
+      text: 'Points per game',
+    },
+    {
+      dataField: 'games_played',
+      text: 'Games played',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container mt-4'>
+      {isLoading ? (
+        <h1>Loading</h1>
+      ) : (
+        <BootstrapTable
+          keyField='name'
+          data={players}
+          columns={columns}
+          pagination={paginationFactory()}
+          filter={filterFactory()}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
